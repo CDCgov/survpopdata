78dd87c3805aae891276e18752a34022f42e2d2f
Mervin Keith Cuadera <40894971+mcuadera@users.noreply.github.com>
2026-02-23 15:12:31 -0500

    ensure correct pop datasource used in POLIS

--- R/load_polis_pop.R
+++ R/load_polis_pop.R
@@ -39,12 +39,11 @@ load_polis_pop <- function(spatial_scale,
   # Filter and include source column
   polis_data <- switch(
     spatial_scale,
-    "ctry" = dplyr::filter(polis_data, !is.na(Admin0GUID) & is.na(Admin1GUID) & is.na(Admin2GUID)) |>
+    "ctry" = dplyr::filter(polis_data, !is.na(Admin0GUID) & is.na(Admin1GUID) & is.na(Admin2GUID) & FK_DataSetId == 1) |>
       dplyr::select(-dplyr::any_of(c("Admin1GUID", "Admin1Name", "Admin1Id",
                                      "Admin2GUID", "Admin2Name", "Admin2Id"))),
-    "prov" = dplyr::filter(polis_data, !is.na(Admin0GUID) & !is.na(Admin1GUID) & is.na(Admin2GUID)) |>
-      dplyr::select(-dplyr::any_of(c("Admin2GUID", "Admin2Name", "Admin2Id"))),
-    "dist" = dplyr::filter(polis_data, !is.na(Admin0GUID) & !is.na(Admin1GUID) & !is.na(Admin2GUID))
+    "prov" = dplyr::filter(polis_data, !is.na(Admin0GUID) & !is.na(Admin1GUID) & is.na(Admin2GUID) & FK_DataSetId == 2),
+    "dist" = dplyr::filter(polis_data, !is.na(Admin0GUID) & !is.na(Admin1GUID) & !is.na(Admin2GUID) & FK_DataSetId == 2)
   ) |>
     dplyr::mutate(datasource = "POLIS API")
 
